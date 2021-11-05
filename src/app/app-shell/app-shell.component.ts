e3379dca4e77bb82185b5bd92b8aa26e9a61d2f4
Kacy Sommers <kacysommers@gmail.com>
2021-11-04 18:53:34 -0700

    add placeholder video to home page

--- src/app/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shell',
   template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
+    <div class="app-container">
+      <app-header></app-header>
+      <div class="app-body-container">
+        <router-outlet></router-outlet>
+      </div>
+      <app-footer></app-footer>
+    </div>
   `,
+  styleUrls: ['./app-shell.component.scss'],
 })
 export class AppShellComponent {}
