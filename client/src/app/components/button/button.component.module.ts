6507feb633cdaac7752e70375f6137cdfc8db807
Kacy Sommers <kacysommers@gmail.com>
2021-11-22 11:00:13 -0800

    button isLoading logic

--- client/src/app/components/button/button.component.module.ts
+++ client/src/app/components/button/button.component.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SpinnerComponentModule } from '../spinner/spinner.component.module';
 import { IconComponentModule } from '../icon/icon.component.module';
 import { ButtonComponent } from './button.component';
 
 @NgModule({
-  imports: [CommonModule, IconComponentModule],
+  imports: [CommonModule, IconComponentModule, SpinnerComponentModule],
   declarations: [ButtonComponent],
   exports: [ButtonComponent],
 })
